4855face2046bed0ee942286d3030070aecc6a4f
Xeno <jurekmikolaj3@gmail.com>
2026-07-05 21:03:05 +0200

    BW 50/HealthSys (#5)
    * feat(player/BW-50): HealtSystem - podstawy
    
    desc: Dodano event bus i eventy odpowiedzialne za zdrowei gracza, oraz metody je obsugujace (damage, heal, died, changed)
    
    * feat(player/BW-50): Resetowanie HP gracza
    
    desc: Dodano metode OnNewGameStarted, ktora resetuje zdrowie gracza do wartosci poczatkowych
    
    * docs(events/BW-50): Dokumentacja Event Bus
    
    * chore(player/BW-50): Obrażenia gracza
    
    desc: podpięto sygnał PlayerTakeDamage do przeciwnika
    
    * refactor(HealthSystem/BW-50): Logi
    
    desc: Dodano logi dla HealthSystem

---
+++ src/player/PlayerHealth.cs
@@ -0,0 +1,61 @@
+using BeyondTheWorlds.autoloads;
+using Godot;
+
+namespace BeyondTheWorlds.player;
+
+public partial class PlayerHealth : Node
+{
+	public short MaxHealth { get; private set; } = 50;
+	
+	/// <summary>
+	/// Zmienna CurrentHealth typu short (int16 bit) przechowuje aktualny stan zdrowia gracza
+	/// i pilnuje, aby nie wyjść poza maksymalny zakres (MaxHealth) poprzez set i get. W przypadku
+	/// gdy HP spadnie do 0, emituje stosowny sygnał, to samo w przypadku zmianny wartości.
+	/// </summary>
+	private short _currentHealth;
+	public short CurrentHealth
+	{
+		get => _currentHealth;
+		private set
+		{
+			_currentHealth = (short) Mathf.Clamp(value, 0, MaxHealth);
+			GD.Print($"HealthSystem: New HP value is {_currentHealth}/{MaxHealth}");
+			Events.EmitPlayerHealthChanged(_currentHealth, MaxHealth);
+			
+			if (_currentHealth == 0)
+			{
+				GD.Print("HealthSystem: Player died!");
+				Events.EmitPlayerDied();
+			}
+		}
+	}
+
+	
+	public override void _Ready()
+	{
+		_currentHealth = MaxHealth;
+		
+		Events.Instance.PlayerHealed += OnPlayerHealed;
+		Events.Instance.PlayerTakedDamage += OnPlayerTakedDamage;
+	}
+
+
+	private void OnPlayerHealed(short value)
+	{
+		GD.Print($"HealthSystem: Healed {value} HP");
+		CurrentHealth += value;
+	}
+
+	private void OnPlayerTakedDamage(short value)
+	{
+		GD.Print($"HealthSystem: Taked {value} Damage");
+		CurrentHealth -= value;
+	}
+
+	private void OnNewGameStarted()
+	{
+		GD.Print("HealthSystem: HP reseted");
+		_currentHealth = 50;
+		MaxHealth = 50;
+	}
+}
