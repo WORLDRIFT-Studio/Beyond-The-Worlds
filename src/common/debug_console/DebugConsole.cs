ba3f81a2c270d6d01e9ab5afbae11bda63ac996f
xeno178531 <kubekkakao6@gmail.com>
2026-07-06 17:40:48 +0200

    docs(Debug/BW-53): Dokumentacja DebugConsole

--- src/common/debug_console/DebugConsole.cs
+++ src/common/debug_console/DebugConsole.cs
@@ -2,7 +2,6 @@ using BeyondTheWorlds.autoloads;
 using Godot;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace BeyondTheWorlds.common.debug_console;
 
@@ -11,29 +10,37 @@ public partial class DebugConsole: Node
     // private string LogFilePath = $"user://logs/btw-logs-{System.DateTime.Now}.log";
     // TODO: Zrobic zapis logow do pliku
 
+    /// <summary>
+    /// Hisotira logów
+    /// </summary>
     private readonly List<String> _logHistory = new();
-    private int _logLine = 1;
     
+    /// <summary>
+    /// Instancja okna, umożliwająca dostęp do niego
+    /// </summary>
+    private Window CurrentWindowInstance { get; set; }
+    
+    /// <summary>
+    /// Instancja umożliwiająca dostęp do skryptu bez wyszkuwania go w drzeiw sceny
+    /// </summary>
     public static DebugConsole Instance { get; private set; }
 
     #region Nodes
-    [ExportCategory("Debug Console")]
-    [ExportGroup("Console Nodes")]
+    /// <summary>
+    /// Zawiera scenę okna konsoli
+    /// </summary>
     [Export] private PackedScene ConsoleWindowScene { get; set; }
+    
     #endregion
-    
-    private Window CurrentWindowInstance { get; set; }
-    
+
+    #region Godot Functions
+
     public override void _Ready()
     {
         Instance = this;
         ProcessMode = ProcessModeEnum.Always;
     }
 
-    public static void Log(string level, string type, string message)
-    {
-        Instance.ConsoleLog(level, type, message);
-    }
 
     public override void _Process(double delta)
     {
@@ -42,55 +49,58 @@ public partial class DebugConsole: Node
             ToggleConsole();
         }
     }
+
+
+    #endregion
     
+
+    #region Comands
+
+    /// <summary>
+    /// Funkcja służąca do przyjmowania i wywoływania komend z konsoli debugowania
+    /// </summary>
+    /// <param name="command">Parametr przyjmuje komendę i na jej podsatwie wykonuje operacje</param>
     private void OnCommandInput(string command)
-    {
-        switch (command)
         {
-            case "TakeDamage":
-                break;
-        }
-    }
-
-    private void SuggestCommand(string[] input)
-    {
-        switch (input[0])
-        {
-            case "pause-game":
-                break;
-            
-        }
-    }
-
-    private void ToggleConsole()
-    {
-        if (CurrentWindowInstance != null && GodotObject.IsInstanceValid(CurrentWindowInstance) && CurrentWindowInstance.Visible)
-        {
-            CurrentWindowInstance.QueueFree();
-            CurrentWindowInstance = null;
-            GD.Print("DebugConsole: Console Closed");
-        }
-        else
-        {
-            CurrentWindowInstance = ConsoleWindowScene.Instantiate<Window>();
-            AddChild(CurrentWindowInstance);
-
-            LineEdit ConsoleInput = CurrentWindowInstance.GetNode<LineEdit>("%ConsoleInput");
-            ConsoleInput.TextSubmitted += OnCommandInput;
-            
-            Log("info", "Console", "Console succesfully opened!");
-            
-            CurrentWindowInstance.PopupCentered(new Vector2I(800, 600));
-            ConsoleInput.GrabFocus();
-            
-            RichTextLabel ConsoleOutput = CurrentWindowInstance.GetNode<RichTextLabel>("%ConsoleOutput");
-            foreach (String oldLog in _logHistory)
+            switch (command)
             {
-                ConsoleOutput.AppendText(oldLog);
+                case "TakeDamage":
+                    break;
             }
         }
+    
+    private void SuggestCommand(string input)
+        {
+            switch (input)
+            {
+                case "pause-game":
+                    break;
+                
+            }
+        }
+    
+
+    #endregion
+
+    #region Logs
+    
+    /// <summary>
+    /// Służy do logowania z zewnątrz
+    /// </summary>
+    /// <param name="level">Poziom logu - INFO, WARNING, ERROR, oraz pozostałe</param>
+    /// <param name="type">Typ logu - skąd pochodzi</param>
+    /// <param name="message">Wiadomość logu</param>
+    public static void Log(string level, string type, string message)
+    {
+        Instance.ConsoleLog(level, type, message);
     }
 
+    /// <summary>
+    /// Funkcja wewnętrzna odpowiedzialna za logowanie zdarzeń
+    /// </summary>
+    /// <param name="level">Poziom logu - INFO, WARNING, ERROR, oraz pozostałe</param>
+    /// <param name="type">Typ logu - skąd pochodzi</param>
+    /// <param name="message">Wiadomość logu</param>
     private void ConsoleLog(string level, string type, string message)
     {
         level = level.ToUpper();
@@ -115,4 +125,43 @@ public partial class DebugConsole: Node
         }
         
     }
+    
+    #endregion
+
+    #region Tools
+
+    private void ToggleConsole()
+        {
+            // Sprawdza czy okno faktycznie nie istnieje i nie zalega w pamięci
+            if (CurrentWindowInstance != null &&
+                GodotObject.IsInstanceValid(CurrentWindowInstance) &&
+                CurrentWindowInstance.Visible)
+            {
+                CurrentWindowInstance.QueueFree();
+                CurrentWindowInstance = null;
+                GD.Print("DebugConsole: Console Closed");
+            }
+            else
+            {
+                CurrentWindowInstance = ConsoleWindowScene.Instantiate<Window>();
+                AddChild(CurrentWindowInstance);
+    
+                LineEdit ConsoleInput = CurrentWindowInstance.GetNode<LineEdit>("%ConsoleInput");
+                ConsoleInput.TextSubmitted += OnCommandInput;
+                
+                Log("info", "Console", "Console succesfully opened!");
+                
+                CurrentWindowInstance.PopupCentered(new Vector2I(800, 600));
+                ConsoleInput.GrabFocus();
+                
+                RichTextLabel ConsoleOutput = CurrentWindowInstance.GetNode<RichTextLabel>("%ConsoleOutput");
+                foreach (String oldLog in _logHistory)
+                {
+                    ConsoleOutput.AppendText(oldLog);
+                }
+            }
+        }
+    
+
+    #endregion
 }
